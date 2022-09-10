e47ee2479f101741162afe9c3e69f8847a6f0c30
yellowzp <yellowhzp@gmail.com>
2022-09-10 12:03:55 +0800

    add webserver

---
+++ demo-webserver/src/test/java/com/example/demowebserver/DemoWebserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demowebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
